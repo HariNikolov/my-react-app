181c0ed0ff4b16f5df8d06cf92b87dc09a4e4825
HariNikolov <hakkonikoloff@gmail.com>
2020-08-16 08:36:56 +0300

    ----

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,9 +14,14 @@ import * as ROUTES from "../../constant/routes";
 import ProductPage from "../ProductPage/ProductPage";
 import Blog from "../BlogPage/BlogPage";
 import CheckoutPage from "../Checkout/CheckoutPage";
+<<<<<<< HEAD
 import Alert from "@material-ui/lab/Alert";
 import ShoppingCart from "../OrderPage/OrderPage";
 import { withFirebase } from "../Firebase";
+=======
+import { withAuthentication } from "../Session";
+import ShoppingCart from "../OrderPage/OrderPage";
+>>>>>>> 63a9747fa550b0f1c04bedb86316c9fe36f1fcf9
 
 const Cover = () => {
   return (
@@ -40,6 +45,7 @@ const Content = () => {
   );
 };
 
+<<<<<<< HEAD
 const isLoggedIn = (
   <Fragment>
     <Route path={ROUTES.CONTACT} component={Contacts} exact />
@@ -58,6 +64,9 @@ const alertMsg = (
 );
 
 const Home = ({ firebase }) => (
+=======
+const Home = () => (
+>>>>>>> 63a9747fa550b0f1c04bedb86316c9fe36f1fcf9
   <Router>
     <React.Fragment>
       <NavigationBar />
@@ -65,11 +74,25 @@ const Home = ({ firebase }) => (
         <Route path={ROUTES.REGISTER} component={Register} exact />
         <Route path={ROUTES.LOGIN} component={Login} exact />
         <Route path={ROUTES.HOME} component={Content} exact />
+<<<<<<< HEAD
       </Switch>
       {firebase.currUser() ? isLoggedIn : alertMsg}
+=======
+        <Route path={ROUTES.CONTACT} component={Contacts} exact />
+        <Route path={ROUTES.COLLECTIONS} component={ProductPage} exact />
+        <Route path={ROUTES.CATALOG} component={Catalog} exact />
+        <Route path={ROUTES.BLOG} component={Blog} exact />
+        <Route path={ROUTES.CHECKOUT} component={CheckoutPage} exact />
+        <Route path={ROUTES.ORDER} component={ShoppingCart} exact />
+      </Switch>
+>>>>>>> 63a9747fa550b0f1c04bedb86316c9fe36f1fcf9
       <Footer />
     </React.Fragment>
   </Router>
 );
 
+<<<<<<< HEAD
 export default withFirebase(Home);
+=======
+export default withAuthentication(Home);
+>>>>>>> 63a9747fa550b0f1c04bedb86316c9fe36f1fcf9
